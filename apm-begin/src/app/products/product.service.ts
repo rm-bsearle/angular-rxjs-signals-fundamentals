b5c6f7c51b4de20b634e67c8a7d07b4e1bc99b6a
rm-bsearle <briansearle@rocket.com>
2026-09-11 11:21:15 -0700

    Handling HTTP Errors in a Service

--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,20 +1,24 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { catchError, Observable, of, tap, throwError } from 'rxjs';
 import { Product } from './product';
+import { ProductData } from './product-data';
+import { HttpErrorService } from '../utilities/http-error.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  private productsUrl = 'api/products';
+  private productsUrl = 'api/productss';
 
   private http = inject(HttpClient);
+  private errorService = inject(HttpErrorService);
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl)
       .pipe(
-        tap(() => console.log('In http.get pipeline'))
+        tap(() => console.log('In http.get pipeline')),
+        catchError(err => this.handleError(err)),
       )
   }
 
@@ -24,5 +28,12 @@ export class ProductService {
       .pipe(
         tap(() => console.log('in http.get product pipeline')),
       )
+  }
+
+  handleError(err: HttpErrorResponse): Observable<never> {
+    const formattedMessage = this.errorService.formatError(err);
+    // can simply throw, in this context it will also create a replacement observable that when subscribed emits an error notification
+    // throw formattedMessage;
+    return throwError(() => new Error(formattedMessage));
   }
 }
