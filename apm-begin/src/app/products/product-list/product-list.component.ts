631ecc243c9635303b0ed87fd31461de9241c770
rm-bsearle <briansearle@rocket.com>
2026-09-21 13:59:13 -0700

    Demo: Caching Retrieved Data

--- apm-begin/src/app/products/product-list/product-list.component.ts
+++ apm-begin/src/app/products/product-list/product-list.component.ts
@@ -19,7 +19,6 @@ export class ProductListComponent {
 
   // Products
   readonly products$ = this.productService.products$.pipe(
-      tap(() => console.log("In component pipeline")),
       catchError(err => {
         this.errorMessage = err;
         return EMPTY;
--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { catchError, map, Observable, of, switchMap, tap, throwError } from 'rxjs';
+import { catchError, map, Observable, of, shareReplay, switchMap, tap, throwError } from 'rxjs';
 import { Product } from './product';
 import { HttpErrorService } from '../utilities/http-error.service';
 import { Review } from '../reviews/review';
@@ -18,7 +18,7 @@ export class ProductService {
 
   readonly products$ = this.http.get<Product[]>(this.productsUrl)
       .pipe(
-        tap(() => console.log('In http.get pipeline')),
+        tap(p => console.log(JSON.stringify(p))),
         catchError(err => this.handleError(err)),
       );
 
